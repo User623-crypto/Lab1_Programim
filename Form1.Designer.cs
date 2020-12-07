11434814c624fbdb0346590a5dfb10f256680f20
User623-crypto <roel.nasufi@outlook.com>
2020-12-07 21:50:17 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ProjektTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.majtas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.djathtas = new System.Windows.Forms.Button();
+            this.lart = new System.Windows.Forms.Button();
+            this.poshte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // majtas
+            // 
+            this.majtas.Location = new System.Drawing.Point(330, 73);
+            this.majtas.Name = "majtas";
+            this.majtas.Size = new System.Drawing.Size(35, 23);
+            this.majtas.TabIndex = 1;
+            this.majtas.Text = "<";
+            this.majtas.UseVisualStyleBackColor = true;
+            this.majtas.Click += new System.EventHandler(this.majtas_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Levizese";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(619, 73);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(180, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Nuk Ben Gje Mos Shtyp";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(619, 132);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(111, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Mos1😴😴😴";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(619, 174);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Mos2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // djathtas
+            // 
+            this.djathtas.Location = new System.Drawing.Point(407, 71);
+            this.djathtas.Name = "djathtas";
+            this.djathtas.Size = new System.Drawing.Size(35, 23);
+            this.djathtas.TabIndex = 6;
+            this.djathtas.Text = ">";
+            this.djathtas.UseVisualStyleBackColor = true;
+            this.djathtas.Click += new System.EventHandler(this.djathtas_Click);
+            // 
+            // lart
+            // 
+            this.lart.Location = new System.Drawing.Point(367, 35);
+            this.lart.Name = "lart";
+            this.lart.Size = new System.Drawing.Size(35, 23);
+            this.lart.TabIndex = 7;
+            this.lart.Text = "↑";
+            this.lart.UseVisualStyleBackColor = true;
+            this.lart.Click += new System.EventHandler(this.lart_Click);
+            // 
+            // poshte
+            // 
+            this.poshte.Location = new System.Drawing.Point(367, 102);
+            this.poshte.Name = "poshte";
+            this.poshte.Size = new System.Drawing.Size(35, 23);
+            this.poshte.TabIndex = 8;
+            this.poshte.Text = "💔";
+            this.poshte.UseVisualStyleBackColor = true;
+            this.poshte.Click += new System.EventHandler(this.poshte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.poshte);
+            this.Controls.Add(this.lart);
+            this.Controls.Add(this.djathtas);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.majtas);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button majtas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button djathtas;
+        private System.Windows.Forms.Button lart;
+        private System.Windows.Forms.Button poshte;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void changeLabelColor()
+        {
+            Random rnd = new Random();
+            this.label1.ForeColor = Color.FromArgb(rnd.Next(0,256), rnd.Next(0, 256), rnd.Next(0, 256));
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void majtas_Click(object sender, EventArgs e)
+        {
+            int speed = 0;
+            try
+            {
+                 speed = int.Parse(this.textBox1.Text);
+                this.label1.Left = this.label1.Left - speed;
+                changeLabelColor();
+
+            }
+            catch(Exception)
+            {
+                label1.Text = "O vella vendos nje numer tek textfield-i";
+            }
+            
+            
+        }
+
+        private void djathtas_Click(object sender, EventArgs e)
+        {
+            int speed = 0;
+            try
+            {
+                speed = int.Parse(this.textBox1.Text);
+                this.label1.Left = this.label1.Left + speed;
+                changeLabelColor();
+
+            }
+            catch (Exception)
+            {
+                label1.Text = "O vella vendos nje numer tek textfield-i";
+            }
+
+        }
+
+        private void lart_Click(object sender, EventArgs e)
+        {
+            int speed = 0;
+            try
+            {
+                speed = int.Parse(this.textBox1.Text);
+                this.label1.Top = this.label1.Top - speed;
+                changeLabelColor();
+
+            }
+            catch (Exception)
+            {
+                label1.Text = "O vella vendos nje numer tek textfield-i";
+            }
+
+        }
+
+        private void poshte_Click(object sender, EventArgs e)
+        {
+            int speed = 0;
+            try
+            {
+                speed = int.Parse(this.textBox1.Text);
+                this.label1.Top = this.label1.Top + speed;
+                changeLabelColor();
+
+            }
+            catch (Exception)
+            {
+                label1.Text = "O vella vendos nje numer tek textfield-i";
+            }
+        }
+    }
+}
